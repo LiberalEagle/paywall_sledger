6f0440d87b30e801625f75334e716825438a68cc
Gannon Barnett <barnettg@mit.edu>
2020-03-06 18:14:01 -0500

    Added capabilities for USA today and fixed html error

--- paywall_sledger.js
+++ paywall_sledger.js
@@ -8,6 +8,8 @@ reg_delete_str = ""
 reg_delete_str += "(paywall)"; //generic (first found in boston globe)
 reg_delete_str += "|(gateway-content)"; //bloomberg
 reg_delete_str += "|(wsw__wg_jzalcb)"; //nytimes 
+reg_delete_str += "|(sp_message)"; //USA today
+reg_delete_str += "|(sp_veil)"; 
 /**
  * Regex edit matcher
  * 
@@ -15,18 +17,17 @@ reg_delete_str += "|(wsw__wg_jzalcb)"; //nytimes
  */
 reg_edit_str = ""
 reg_edit_str += "(css-mcm29f)"; //nytimes 
+reg_edit_str += "|(fancy-box)"; //nat geo
 const paywall_delete_regex = new RegExp(reg_delete_str); 
 const paywall_edit_regex = new RegExp(reg_edit_str); 
 const all_elements = document.getElementsByTagName("*");
 
-console.log(all_elements); 
 for (const element of all_elements) {
     try {
         //Remove classes that might have to do with paywall
         if (element.className) {
             const element_classes = element.className.split(" "); 
             if (element_classes.filter((class_name) => paywall_delete_regex.test(class_name)).length !== 0) {
-                console.log("[paywall_sledger] Removing element with classes \"" + element_classes + "\"."); 
                 element.parentNode.removeChild(element); 
             } 
         }
@@ -36,13 +37,11 @@ for (const element of all_elements) {
         for (const attr of element_attributes) {
             //Remove the attribute if the name has to do with a paywall
             if (paywall_edit_regex.test(attr.name) | paywall_edit_regex.test(attr.value)) {
-                console.log("[paywall_sledger] Removing attribute with name \"" + attr.name + "\" and value \"" + attr.value + "\""); 
                 element.removeAttribute(attr.name); 
             }
             //Remove entire element if an attribute is set to a paywall
             if (paywall_delete_regex.test(attr.value)) {
                 try {
-                    console.log("[paywall_sledger] Removing element with attribute of name \"" + attr.name + "\" and value \"" + attr.value + "\""); 
                     element.parentNode.removeChild(element); 
                 } catch (error) {
                     console.log(error); 
@@ -56,14 +55,15 @@ for (const element of all_elements) {
 
 //Remove all scroll blocks on the page 
 try {
-    const scroll_block_name_regex = new RegExp("(overflow)|(position)|(height)"); 
+    const scroll_block_name_regex = new RegExp("(overflow)|(position)|(height)|(overflow-y)"); 
     const body_style_parts = document.body.getAttribute("style").split(";"); 
     let safe_parts = body_style_parts.filter((part) => !scroll_block_name_regex.test(part)); 
     document.body.setAttribute("style", safe_parts.join(";"));
 
-    const doc_style_parts = document.getAttribute("style").split(";"); 
+    const doc_style_parts = document.getElementsByTagName("html")[0].getAttribute("style").split(";"); 
     doc_safe_parts = doc_style_parts.filter((part) => !scroll_block_name_regex.test(part)); 
-    document.setAttribute("style", doc_safe_parts.join(";"));
+    document.getElementsByTagName("html")[0].setAttribute("style", doc_safe_parts.join(";"));
+    
 } catch (error) {
     console.log(error); 
 }
